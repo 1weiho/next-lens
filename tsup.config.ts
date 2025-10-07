d3a628d6b09b6c47bd5465f9f8b7db680c5d76f6
Yiwei Ho <davidho0403@gmail.com>
2025-10-07 22:41:39 +0800

    chore: use tsup to build

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  target: "node20",
+  outDir: "dist",
+  splitting: false,
+  skipNodeModulesBundle: true,
+});
